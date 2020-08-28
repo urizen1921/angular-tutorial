9aff8d449d42198636077e000ee8ab17579e050e
Paulo Paula <paulo.almeida.paula@gmail.com>
2020-08-28 19:07:28 +0100

    bindings are done, going to directives

---
+++ angular-tutorial/src/app/server/dist/server.component.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.ServerComponent = void 0;
+var ServerComponent = /** @class */ (function () {
+    function ServerComponent() {
+    }
+    return ServerComponent;
+}());
+exports.ServerComponent = ServerComponent;
